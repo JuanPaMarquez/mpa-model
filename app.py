e356e62dc55ac894ffee5df9dbee248e1f98c0e5
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2024-11-24 22:18:50 -0500

    config port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -61,4 +62,5 @@ def evaluar_estudiantes():
   return jsonify(resultados)  # Devolver la lista de resultados
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
